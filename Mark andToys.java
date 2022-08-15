0df44c209ac1efb1cb59d358e5819490f363c161
bharathcsmys <79088828+bharathcsmys@users.noreply.github.com>
2022-08-15 21:45:04 +0530

    Done

--- Mark andToys.java
+++ Mark andToys.java
@@ -1,55 +1,26 @@
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
-class Result {
-
-    /*
-     * Complete the 'maximumToys' function below.
-     *
-     * The function is expected to return an INTEGER.
-     * The function accepts following parameters:
-     *  1. INTEGER_ARRAY prices
-     *  2. INTEGER k
-     */
-
-    public static int maximumToys(List<Integer> prices, int k) {
-    // Write your code here
-
-    }
-
-}
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        int n = Integer.parseInt(firstMultipleInput[0]);
-
-        int k = Integer.parseInt(firstMultipleInput[1]);
-
-        String[] pricesTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-
-        List<Integer> prices = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            int pricesItem = Integer.parseInt(pricesTemp[i]);
-            prices.add(pricesItem);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        int[] toys = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            toys[i] = input.nextInt();
         }
-
-        int result = Result.maximumToys(prices, k);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
+        Arrays.sort(toys);
+        int totalToys = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            k -= toys[i];
+            if(k > 0) totalToys++;
+            if(k <= 0) break;
+        }
+        System.out.println(totalToys);
     }
 }
